590236398d92702b18b380081fcb7d1f45a4a1f4
brighty1982 <ambright_1@hotmail.com>
2016-08-21 20:15:14 +0100

    Add fishing and football page handlers

--- hello.go
+++ hello.go
@@ -5,11 +5,17 @@ import (
     "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])  //write I love /pagename
+func fishingHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, this is the fishing page! %s", r.URL.Path[1:])  //write I love /pagename
+}
+
+func footballHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, this is the football page! %s", r.URL.Path[1:])  //write I love /pagename
 }
 
 func main() {
-    http.HandleFunc("/", handler)   //handle all requests to webroot with handler function
+    http.HandleFunc("/fishingHandler", fishingHandler)   //fishing page handler
+    http.HandleFunc("/footballHandler", footballHandler)
+    
     http.ListenAndServe(":8080", nil)   //listen on port 8080
 }
