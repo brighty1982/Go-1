ad30fc3fb61e7c89e671c602cec672abfa32cbe5
brighty1982 <ambright_1@hotmail.com>
2016-08-21 20:21:08 +0100

    tidied comments

--- hello.go
+++ hello.go
@@ -5,17 +5,19 @@ import (
     "net/http"
 )
 
+//fishing handler
 func fishingHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, this is the fishing page! %s", r.URL.Path[1:])  //write I love /pagename
 }
 
+//football handler
 func footballHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, this is the football page! %s", r.URL.Path[1:])  //write I love /pagename
+    fmt.Fprintf(w, "Hi there, this is the football page! %s", r.URL.Path[1:]) 
 }
 
 func main() {
-    http.HandleFunc("/fishingHandler", fishingHandler)   //fishing page handler
-    http.HandleFunc("/footballHandler", footballHandler)
+    http.HandleFunc("/fishingHandler", fishingHandler)   //register fishing page handler
+    http.HandleFunc("/footballHandler", footballHandler) //register football page handler
     
     http.ListenAndServe(":8080", nil)   //listen on port 8080
 }
